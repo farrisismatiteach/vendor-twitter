6b3c8d0d7366a43fcba9628cdefb628055192472
farrisismatiteach <farris.ismati.teach@gmail.com>
2023-02-11 18:28:58 -0500

    fix container name

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -94,7 +94,7 @@ resource "aws_ecs_service" "ecs" {
 
   load_balancer {
     target_group_arn = aws_lb_target_group.target_group.arn
-    container_name = "${var.app_name}-container"
+    container_name = "${var.app_name}-td"
     container_port = 80
   }
 
