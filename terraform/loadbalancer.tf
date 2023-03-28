d028873a574ae516e99a0bdfb3b7650d20c8788f
farrisismatiteach <farris.ismati.teach@gmail.com>
2023-03-27 21:50:27 -0400

    removed accident x

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "lb_sg" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0x"]
+    cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 }
